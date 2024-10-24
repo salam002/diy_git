39ba64ffdf84b2e81b0dbc4292cccc74d8dbd73a
salam002 <salam2@berkeley.edu>
2024-10-24 14:37:52 -0700

    adding repo_path, repo_file, repo_dir, and constructor functions

--- libwyag.py
+++ libwyag.py
@@ -1,3 +1,4 @@
+# tutorial from https://wyag.thb.lt/
 import argparse
 import collections
 import configparser
@@ -36,3 +37,59 @@ def main(argv=sys.argv[1:]):
         case "status"       : cmd_status(args)
         case "tag"          : cmd_tag(args)
         case _              : print("Bad command.")
+
+# abstractions
+
+# creating new repo object: 2 checks
+    # must verify that the directory exists, and contains subdirectory .git
+    # read its configuration in .git/config (INI file) and control that core.repositoryformatversion is 0
+class GitRepository(object):
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def _init_(self, path, force=False): # optional force which disables all checks - repo_create() function uses Repository object to create the repo
+
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.getdir)):
+            raise Exception("Not a Git repository %s" % path)
+        
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        
+        elif not force:
+            raise Exception("Config file missing")
+        
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsupported repositoryformatversion %s" % vers)
+
+# path building function
+def repo_path(repo, *path):
+    return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    path = repo_path(repo, *path)
+    
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+        
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
